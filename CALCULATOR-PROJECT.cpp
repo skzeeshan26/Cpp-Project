69d0d4da4aa1e66c64778bf1002f164d94e7458e
shkabdullah0985-cmyk <shkabdullah0985@gmail.com>
2025-11-01 21:23:01 +0530

    Refactor calculator output messages
    Updated output messages for clarity and fixed formatting issues.

--- CALCULATOR-PROJECT.cpp
+++ CALCULATOR-PROJECT.cpp
@@ -7,22 +7,22 @@ int main() {
     cin>>a>>op>>b;
     switch (op){
         case '+':
-        cout<<"sum is "<<a+b;
+        cout<<"sum is "<<a+b;   \\if user will select addition operator it will perform addition
         break;
         case '-':
-        cout<<"sub is "<<a-b;
+        cout<<"sub is "<<a-b;  \\if user will select subtraction operator it will perform subtraction
         break;
         case '*':
-        cout<<"mul is"<<a*b;
+        cout<<"mul is"<<a*b;    \\if user will select multiplication operator it will perform multiplication
         break;
         case '/':
         if(b==0) {
-            cout<<"number is 0 number is invalid";
+            cout<<"number is 0 number is invalid"; 
         }
         else {
-        cout<<"div is "<<a/b;
+        cout<<"div is "<<a/b;     \\if user will select divison operator it will perform division
         }break;
-        default:
+        default: \\if user will type invalid operation
             cout << "Enter a vaild opreation";
         break;
     }
